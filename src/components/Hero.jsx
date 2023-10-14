c7bbc7588f4b033acee729116527407f632da855
Maan <maan.neami@gmail.com>
2023-10-15 00:23:32 +0300

    changed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         </h1>
         <h2 className='desc'>
             Simplify your reading with Summize, an open-source article summarizer
-            that transforms lengthy articles into clear and concise summaries
+            that transforms lengthy articles into clear and concise summaries using GPT-4
         </h2>
     </header>
   )
